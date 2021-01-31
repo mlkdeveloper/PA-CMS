00eb39e58b634ad2c2744a6227c6daa35efb2975
samy sab <samy.saberi@gmail.com>
2021-01-31 20:06:17 +0100

    MaJ scss

--- www/Views/templates/front.tpl.php
+++ www/Views/templates/front.tpl.php
@@ -4,11 +4,17 @@
 	<meta charset="UTF-8">
 	<title>Template de front</title>
 	<meta name="description" content="description de la page de front">
+    <link rel="stylesheet" href="../../dist/main.css">
 </head>
 <body>
-	<header>
-		<h1>Template de front</h1>
-	</header>
+<header>
+    <div class="sidenav">
+        <a href="#about">About</a>
+        <a href="#services">Services</a>
+        <a href="#clients">Clients</a>
+        <a href="#contact">Contact</a>
+    </div>
+</header>
 
 	<!-- afficher la vue -->
 	<?php include $this->view ?>
