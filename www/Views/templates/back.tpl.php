7842420ff497431c9d10afd8c5b8b0f14ab1c21e
Théo Sigaud <sigaudtheo@gmail.com>
2021-02-06 20:55:20 +0100

    Add viewport

--- www/Views/templates/back.tpl.php
+++ www/Views/templates/back.tpl.php
@@ -4,6 +4,8 @@
     <meta charset="UTF-8">
     <title><?= $title ?></title>
     <meta name="description" content="description de la page de back">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
     <script src="../../src/js/jquery-3.5.1.min.js"></script>
     <script src="https://cdn.datatables.net/1.10.23/js/jquery.dataTables.min.js"></script>
 
--- www/Views/templates/front.tpl.php
+++ www/Views/templates/front.tpl.php
@@ -4,8 +4,10 @@
 	<meta charset="UTF-8">
 	<title>Template de front</title>
 	<meta name="description" content="description de la page de front">
-    <link rel="stylesheet" href="../../dist/main.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="../../dist/main.css">
 </head>
 <body>
 
