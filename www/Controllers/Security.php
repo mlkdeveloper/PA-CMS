fb4eba7454177b8900302d941f608a1885d005e5
samy sab <samy.saberi@gmail.com>
2021-02-06 11:11:50 +0100

    Nvlle route + template installation

--- www/Controllers/Security.php
+++ www/Controllers/Security.php
@@ -4,7 +4,19 @@
 namespace App\Controller;
 
 
+use App\Core\View;
+
 class Security
 {
+    //Method : Action
+    public function registerInstallAction(){
 
+
+        //Affiche la vue home intégrée dans le template du front
+        $view = new View("installRegister", "install");
+
+
+
+
+    }
 }
---
+++ www/Views/templates/install.tpl.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Template d'installation</title>
+    <meta name="description" content="description de la page de front">
+    <link rel="stylesheet" href="../../dist/main.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<body>
+
+
+<!-- afficher la vue -->
+<?php include $this->view ?>
+
+
+
+</body>
+
+</html>
