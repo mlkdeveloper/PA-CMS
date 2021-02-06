4757533b8d423ce82e35599d8a46e32a88e1d365
Théo Sigaud <sigaudtheo@gmail.com>
2021-02-07 00:42:48 +0100

    Test form

--- www/Views/install.view.php
+++ www/Views/install.view.php
@@ -1,41 +1,43 @@
-<div class="row">
-    <div class="container">
-        <div class="col-lg-6 col-md-12 col-sm-12 col">
+<div class="container">
+    <div class="row">
+        <div class="col-lg-8 col-md-8 col-sm-12 col center-margin">
             <div class="jumbotron">
                 <h4>Bienvenue dans l’installation de click & collect !
                     Vous n’avez qu’a remplir les informations demandées ci-dessous et vous serez prêt à utiliser Click &
                     Create
                 </h4>
 
-                <div class="container-input">
-                    <label>toto</label>
+                <div class="form_align center-margin container-input">
+                    <label>Nom de la base de données</label>
                     <input type="text">
-                    <p>Description</p>
+                    <p>Le nom de la base de donnée avec laquelle vous souhaitez utiliser votre Click & Create</p>
                 </div>
-                <div class="container-input">
-                    <label>toto</label>
+                <div class="form_align center-margin container-input">
+                    <label>Identidiant</label>
                     <input type="text">
-                    <p>Description</p>
+                    <p>Votre identifiant MySQL</p>
 
                 </div>
-                <div class="container-input">
-                    <label>toto</label>
+                <div class="form_align center-margin container-input">
+                    <label>Mot de passe</label>
                     <input type="text">
-                    <p>Description</p>
+                    <p>Votre mot de passe de basse de données</p>
 
                 </div>
-                <div class="container-input">
-                    <label>toto</label>
+                <div class="form_align center-margin container-input">
+                    <label>Adresse de la base de données</label>
                     <input type="text">
-                    <p>Description</p>
                 </div>
-                <div class="container-input">
-                    <label>toto</label>
+                <div class="form_align center-margin container-input">
+                    <label>Port</label>
                     <input type="text">
-                    <p>Description</p>
+                </div>
+                <div class="form_align center-margin container-input">
+                    <label>Préfixe des tables</label>
+                    <input type="text">
                 </div>
                 <?php echo DBHOST;
-                echo 'tdt'?>
+                echo 'tdt' ?>
             </div>
         </div>
     </div>
