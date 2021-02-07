dcfa5cfba9abdd7285533f54bbada9878306fa47
Théo Sigaud <sigaudtheo@gmail.com>
2021-02-07 15:37:06 +0100

    Delete files .env

--- www/Controllers/Security.php
+++ www/Controllers/Security.php
@@ -1,5 +1,6 @@
 <?php
 namespace App\Controller;
+use App\Core\ConstantManager;
 use App\Core\View;
 
 session_start();
@@ -25,29 +26,43 @@ class Security
 
     public function startInstallAction(){
 
-        if(count($_POST) != 6){
-            echo "toto";
+        $dataArray = $this->checkInformations($_POST);
+        print_r($dataArray);
+        new ConstantManager();
+
+//        file_get_contents("/path/to/your/file/edit.json", true);
+//        fopen('/.env', "r");
+    }
+
+
+
+    private function checkInformations($data){
+        if(count($data) != 6){
             $_SESSION['securityInstall'] = 'Formulaire non conforme';
             header('Location: /');
         }else{
-            $erros = [];
 
-            if (empty($_POST['name_bdd'])
-            ||empty($_POST['user_bdd'])
-            ||empty($_POST['pwd_bdd'])
-            ||empty($_POST['address_bdd'])
-            ||empty( $_POST['port_bdd'])
-            ||empty($_POST['prefixe_bdd'])){
+            if (empty($data['name_bdd'])
+                ||empty($data['user_bdd'])
+                ||empty($data['pwd_bdd'])
+                ||empty($data['address_bdd'])
+                ||empty($data['port_bdd'])
+                ||empty($data['prefixe_bdd'])){
+
                 $_SESSION['securityInstall'] = "Veuillez remplir tous les champs";
                 header('Location: /');
             }
 
+            $dataArray = [];
 
+            $dataArray['name_bdd'] = trim($data['user_bdd']);
+            $dataArray['user_bdd'] = trim($data['user_bdd']);
+            $dataArray['pwd_bdd'] = trim($data['pwd_bdd']);
+            $dataArray['address_bdd'] = trim($data['address_bdd']);
+            $dataArray['port_bdd'] = trim($data['port_bdd']);
+            $dataArray['prefixe_bdd'] = trim($data['prefixe_bdd']);
 
+            return $dataArray;
         }
-
-//        file_get_contents('/test/fzef/azeqfzeenv', false);
-//        file_get_contents("/path/to/your/file/edit.json", true);
-        fopen('/.env', "r");
     }
 }
