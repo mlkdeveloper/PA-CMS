cd81254500c363fe561889a2f71a2a5602dd46e6
Théo Sigaud <sigaudtheo@gmail.com>
2021-02-13 20:05:37 +0100

    Add all blocks

--- www/public/js/publisher/publisher.js
+++ www/public/js/publisher/publisher.js
@@ -1,20 +1,48 @@
-function addBloc(col){
-    console.log(col);
-    if (col > 12){
+function addBloc(col) {
+    var html = '<section class="container">' +
+        '<div class="row">';
 
-    }else{
-        var html = '<section class="container">' +
-            '<div class="row">';
-                for (var i = 0; i < 12; i+=col){
-                    html += '<div class="col-lg-'+col+' col-md-'+col+' col-sm-12 col center-margin">' +
+    if (col > 12) {
+        switch (col){
+            case 39:
+                html += '<div class="col-lg-3 col-md-3 col-sm-12 col">' +
+                        '<div class="jumbotron blocEdit">' +
+                            '<h1>Test</h1>' +
+                        '</div>' +
+                    '</div>' +
+                    '<div class="col-lg-9 col-md-9 col-sm-12 col">' +
                         '<div class="jumbotron blocEdit">' +
                             '<h1>Test</h1>' +
                         '</div>' +
                     '</div>';
-                }
-            html += '</div>' +
+                break;
+            case 93:
+                html += '<div class="col-lg-9 col-md-9 col-sm-12 col">' +
+                        '<div class="jumbotron blocEdit">' +
+                            '<h1>Test</h1>' +
+                        '</div>' +
+                    '</div>' +
+                    '<div class="col-lg-3 col-md-3 col-sm-12 col">' +
+                        '<div class="jumbotron blocEdit">' +
+                            '<h1>Test</h1>' +
+                        '</div>' +
+                    '</div>';
+                break;
+        }
+    } else {
+
+        for (var i = 0; i < 12; i += col) {
+            html += '<div class="col-lg-' + col + ' col-md-' + col + ' col-sm-12 col">' +
+                    '<div class="jumbotron blocEdit">' +
+                        '<h1>Test</h1>' +
+                    '</div>' +
+                '</div>';
+        }
+
+    }
+
+    html += '</div>' +
         '</section>';
 
-        $('#createBloc').before(html);
-    }
+    $('#createBloc').before(html);
 }
