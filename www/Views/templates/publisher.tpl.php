b0d9737abe52b22924f272b4f37309026fbc0719
Théo Sigaud <sigaudtheo@gmail.com>
2021-02-13 01:14:20 +0100

    Add hover on img

--- www/Views/templates/publisher.tpl.php
+++ www/Views/templates/publisher.tpl.php
@@ -7,6 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <script src="../../src/js/jquery-3.5.1.min.js"></script>
+    <script src="../../src/js/publisher.js"></script>
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css">
     <link rel="stylesheet" href="../../dist/main.css">
