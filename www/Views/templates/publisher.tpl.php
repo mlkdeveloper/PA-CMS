92600a27b4e121fbbab70199f8f97f5b8acf2fef
Théo Sigaud <sigaudtheo@gmail.com>
2021-02-15 00:24:33 +0100

    Add tiny local

--- www/Views/templates/publisher.tpl.php
+++ www/Views/templates/publisher.tpl.php
@@ -7,7 +7,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
         <script src="../../src/js/jquery-3.5.1.min.js"></script>
-        <script src="https://cdn.tiny.cloud/1/hhyltwzrx7o3enydpiz3vfnppsdw0hg6iyjr8ruqxp8gfq9i/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
+        <script src="../../public/tinymce/tinymce.min.js"></script>
         <script src="../../public/js/publisher/publisher.js"></script>
 
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css">
