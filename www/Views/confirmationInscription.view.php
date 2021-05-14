4677ca0443ed24b3b8ad2895ab6a1f80aacc07b0
samy sab <samy.saberi@gmail.com>
2021-05-14 11:28:02 +0200

    fix de la page de confirmation inscription

--- www/Views/confirmationInscription.view.php
+++ www/Views/confirmationInscription.view.php
@@ -1,1 +1,17 @@
-<h1>Inscription Confirmé !!!</h1>
+<div style="left: 0">
+    <h1 style="text-align: center;font-size: 40px">Confirmation d'inscription</h1>
+    <div class="alert alert--green">
+        <h2>Feélicitation !</h2>
+        <p>Votre compte vien d'être vérifié, vous pouvez désormais profitez pleinement de toute les fonctionnalités de C&C Collect ! </p>
+    </div>
+    
+</div>
+
+
+<style>
+    .container-body{
+        left: 0;
+        margin-left: auto;
+        margin-right: auto;
+    }
+</style>
