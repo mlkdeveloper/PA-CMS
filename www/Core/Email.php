ef65db580ad6803327f8832aa01e065beb89a829
Théo Sigaud <sigaudtheo@gmail.com>
2021-06-08 15:44:29 +0200

    Add PHPMailer

--- www/Core/Email.php
+++ www/Core/Email.php
@@ -253,7 +253,7 @@ class Email
                   <tr>
                     <td style=\"padding-right: 0px;padding-left: 0px;\" align=\"center\">
                       
-                      <img align=\"center\" border=\"0\" src=\"../images/image-1.png\" alt=\"Image\" title=\"Image\" style=\"outline: none;text-decoration: none;-ms-interpolation-mode: bicubic;clear: both;display: inline-block !important;border: none;height: auto;float: none;width: 39%;max-width: 206.7px;\" width=\"206.7\" class=\"v-src-width v-src-max-width\"/>
+                      <img align=\"center\" border=\"0\" src=\"./images/image-1.png\" alt=\"Image\" title=\"Image\" style=\"outline: none;text-decoration: none;-ms-interpolation-mode: bicubic;clear: both;display: inline-block !important;border: none;height: auto;float: none;width: 39%;max-width: 206.7px;\" width=\"206.7\" class=\"v-src-width v-src-max-width\"/>
                       
                     </td>
                   </tr>
