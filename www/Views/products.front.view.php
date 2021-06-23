def8aabd4a1a7d3bebc5b7ad8de5c95c42c1c0ef
waseem1104 <waseem11@hotmail.fr>
2021-06-23 09:57:44 +0200

    Pagination component

--- www/Views/products.front.view.php
+++ www/Views/products.front.view.php
@@ -16,7 +16,7 @@
             <?php
             foreach ($products as $product):
             ?>
-            <div class="col-md-3">
+            <div class="col-lg-3 col-md-3  col-sm-6 col">
                 <div class="card">
                     <div class="card-body">
                         <h3 class="card-title"> <?= $product['name'] ?></h3>
@@ -32,23 +32,24 @@
 </section>
 
 <section>
-
+    <div class="container">
     <nav>
         <ul class="pagination">
-            <!-- Lien vers la page précédente (désactivé si on se trouve sur la 1ère page) -->
+
             <li class="page-item <?= ($page == 1) ? "disabled" : "" ?>">
                 <a href="/collections?name=<?= $name ?>&page=<?= $page - 1 ?>" class="page-link">Précédente</a>
             </li>
             <?php for($nbpage = 1; $nbpage <= $pages; $nbpage++): ?>
-                <!-- Lien vers chacune des pages (activé si on se trouve sur la page correspondante) -->
+
                 <li class="page-item <?= ($page == $nbpage) ? "active" : "" ?>">
                     <a href="./?page=<?= $nbpage ?>" class="page-link"><?= $nbpage ?></a>
                 </li>
             <?php endfor ?>
-            <!-- Lien vers la page suivante (désactivé si on se trouve sur la dernière page) -->
+
             <li class="page-item <?= ($page == $pages) ? "disabled" : "" ?>">
                 <a href="/collections?name=<?= $name ?>&page=<?= $page + 1 ?>" class="page-link">Suivante</a>
             </li>
         </ul>
     </nav>
+    </div>
 </section>
