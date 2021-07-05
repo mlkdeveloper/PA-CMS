adf5b0d10be29594739aef7dc84167ec80923149
Théo Sigaud <sigaudtheo@gmail.com>
2021-07-06 00:10:56 +0200

    Fix dropdowns

--- www/public/js/dropdowns.js
+++ www/public/js/dropdowns.js
@@ -1,20 +1,20 @@
-var dropdown = document.getElementsByClassName("dropdown-btn");
-var i;
-
-for (i = 0; i < dropdown.length; i++) {
-    dropdown[i].addEventListener("click", function () {
-        this.classList.toggle("active");
-        var dropdownContent = this.nextElementSibling;
-        if (dropdownContent.style.display === "flex") {
-            dropdownContent.style.display = "none";
-            $("nav ul a i").css({
-                    transform: "rotate(0deg)",
-            })
-        } else {
-            dropdownContent.style.display = "flex";
-            $("nav ul a i").css({
-                transform: "rotate(180deg)",
-            })
-        }
+$(document).ready(function() {
+    $("#dropdownProducts").on("click", function () {
+        startDropdown(this);
     });
+
+    $("#dropdownSettings").on("click", function () {
+        startDropdown(this);
+    });
+});
+
+
+function startDropdown(dropDown){
+    dropDown.classList.toggle("active");
+    var dropdownContent = dropDown.nextElementSibling;
+    if (dropdownContent.style.display === "flex") {
+        dropdownContent.style.display = "none";
+    } else {
+        dropdownContent.style.display = "flex";
+    }
 }
