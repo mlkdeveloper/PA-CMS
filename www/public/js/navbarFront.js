7316238b94f460b191bc5e52b7d8b2fa8668563a
Théo Sigaud <sigaudtheo@gmail.com>
2021-07-22 15:09:44 +0200

    Fix navbar front

---
+++ www/public/js/navbarFront.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+    $("#hamburger").on("click", function () {
+        navbar();
+    });
+});
+
+
+function navbar(){
+    if ($("#main-nav").css("left") !== "0px"){
+        $('#main-nav').animate({
+            left: 0
+        });
+    }else{
+        $('#main-nav').animate({
+            left: "-100%"
+        });
+    }
+}
