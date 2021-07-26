01d1c4f17dcbd51bbc035099f0b1eca9285a80ed
suvithayah <suvithayah@gmail.com>
2021-07-26 12:50:45 +0200

    Add mandatory asterix for inputs

--- www/Views/updateClient.back.view.php
+++ www/Views/updateClient.back.view.php
@@ -24,43 +24,43 @@
                         <div class=" align  jumbotron form">
                             <div class="col-lg-6 col-md-6 col-sm-12 col">
                                 <div class="form_align form_align--top">
-                                    <label class="label">Nom</label>
+                                    <label class="label">Nom *</label>
                                     <input class="input" type="text" name="lastName" placeholder="Nom" required="required" value="<?php if (isset($values)) echo $values['lastname']; ?>">
                                 </div>
 
                                 <div class="form_align form_align--top">
-                                    <label class="label">Email</label>
+                                    <label class="label">Email *</label>
                                     <input class="input" type="email" name="email" placeholder="Email"  required="required" value="<?php if (isset($values)) echo $values['email']; ?>" >
                                 </div>
 
                                 <div class="form_align form_align--top">
-                                    <label class="label">Adresse</label>
+                                    <label class="label">Adresse *</label>
                                     <input class="input" type="text" name="address" placeholder="Adresse"  required="required" value="<?php if (isset($values)) echo $values['address']; ?>" >
                                 </div>
                                 <div class="form_align form_align--top">
-                                    <label class="label">Code postal</label>
+                                    <label class="label">Code postal *</label>
                                     <input class="input" type="text" name="zipCode" placeholder="Code postal"  required="required" value="<?php if (isset($values)) echo $values['zipcode']; ?> ">
                                 </div>
                             </div>
 
                             <div class="col-lg-6 col-md-6 col-sm-12 col">
                                 <div class="form_align form_align--top">
-                                    <label class="label">Prénom</label>
+                                    <label class="label">Prénom *</label>
                                     <input class="input" type="text" name="firstName" placeholder="Prénom" required="required" value="<?php if (isset($values)) echo $values['firstname']; ?>">
                                 </div>
 
                                 <div class="form_align form_align--top">
-                                    <label class="label">Téléphone</label>
+                                    <label class="label">Téléphone *</label>
                                     <input class="input" type="text" name="phoneNumber" placeholder="Téléphone"  required="required" value="<?php if (isset($values)) echo $values['phoneNumber']; ?>">
                                 </div>
 
                                 <div class="form_align form_align--top">
-                                    <label class="label">Ville</label>
+                                    <label class="label">Ville *</label>
                                     <input class="input" type="text" name="city" placeholder="Ville"  required="required" value="<?php if (isset($values)) echo $values['city']; ?>">
                                 </div>
 
                                 <div class="form_align form_align--top">
-                                    <label class="label">Pays</label>
+                                    <label class="label">Pays *</label>
                                     <input class="input" type="text" name="country" placeholder="Pays"  required="required" value="<?php if (isset($values)) echo $values['country']; ?>" >
                                 </div>
 
