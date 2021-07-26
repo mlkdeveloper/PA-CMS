d2e05b818ac51b5db131edb1e50e4536de5cca61
suvithayah <suvithayah@gmail.com>
2021-07-27 01:09:35 +0200

    Fix spelling miskates in emails and utf decode, for accents and some error messages

--- www/Views/confirmationInscription.view.php
+++ www/Views/confirmationInscription.view.php
@@ -1,8 +1,8 @@
 <div style="left: 0">
     <h1 style="text-align: center;font-size: 40px">Confirmation d'inscription</h1>
     <div class="alert alert--green">
-        <h2>Feélicitation !</h2>
-        <p>Votre compte vien d'être vérifié, vous pouvez désormais profitez pleinement de toute les fonctionnalités de C&C Collect ! </p>
+        <h2>Félicitation !</h2>
+        <p>Votre compte a été créé avec succès </p>
     </div>
     
 </div>
